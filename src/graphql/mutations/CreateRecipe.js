298a312376d61395808eb51eb72651f1dfffd604
Luke O'Malley <Lukeomalley95@gmail.com>
2019-10-29 19:38:56 -0400

    created Nav component and added createRecipe mutation and listRecipes query

---
+++ src/graphql/mutations/CreateRecipe.js
@@ -0,0 +1,12 @@
+import gql from 'graphql-tag';
+
+export default gql`
+  mutation createRecipe($name: String!, $ingredients: [String!], $instructions: [String!]) {
+    createRecipe(input: { name: $name, ingredients: $ingredients, instructions: $instructions }) {
+      id
+      name
+      instructions
+      ingredients
+    }
+  }
+`;
